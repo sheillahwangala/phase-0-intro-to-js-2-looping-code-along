0c950af03a82e5accbcdb049c530153251c1fd52
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-08-21 18:36:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(arrayNames, eventName) {
+    let newArrayNames = [];
+    for (let i=0; i<arrayNames.length; i++) {
+        newArrayNames.push(`Thank you, ${arrayNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArrayNames;
+}
+
+function countDown() {
+    let count = 0;
+    while (count < 11) {
+        console.log (count++);
+    }
+}
